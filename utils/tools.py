f504b51e211ab5092df0dbfc613581764d1c36c5
Vadik Alperovich <vadikalperovich@mail.ru>
2020-04-17 21:10:46 +0300

    added observations to stat function

--- utils/tools.py
+++ utils/tools.py
@@ -72,58 +72,62 @@ def generate_stat(algorithms,
 
     return pd.DataFrame.from_dict(info_dct)
 
+
 def generate_stat_for_benchmarks(algorithms,
-                  set_params,
-                  files_w,
-                  files_t,
-                  path_to_benchmarks,
-                  **params):
+                                  set_params,
+                                  files_w,
+                                  files_t,
+                                  path_to_benchmarks,
+                                  n_observations=1,
+                                  **params):
 
     assert len(files_w) == len(files_t)
-    
-    numb_of_files = len(files_w)
-    
     for algorithm in algorithms:
         assert isinstance(algorithm(''), Algorithm)
 
+    numb_of_files = len(files_w)
     info_dct = {
                      'algorithm':     [],
+                     'filename':      [],
                      'reference_len': [],
                      'candidate_len': [],
                      'preprocessing': [],
                      'execution':     [],
-     #                'observation':   [],
+                     'observation':   [],
                      'indexes':       []
                    }
+    for observation in range(n_observations):
 
-    for i in range(numb_of_files):
-        with open(path_to_benchmarks + files_t[i], 'r', encoding='utf-8') as file_t:
-            file_t = file_t.read()
-        
-        with open(path_to_benchmarks + files_w[i], 'r', encoding='utf-8') as file_w:
-            file_w = file_w.read()
+        for i in range(numb_of_files):
 
-        reference = file_t
-        candidate = file_w
+            with open(path_to_benchmarks + files_t[i], 'r', encoding='utf-8') as file_t:
+                file_t = file_t.read()
 
-        for algorithm, params in zip(algorithms, set_params):
+            with open(path_to_benchmarks + files_w[i], 'r', encoding='utf-8') as file_w:
+                file_w = file_w.read()
 
-            start_time = datetime.now()
-            alg = algorithm(reference)
-            alg.set_candidate(candidate, **params)
-            preprocess = datetime.now() - start_time
+            reference = file_t
+            candidate = file_w
 
-            start_time = datetime.now()
-            indexes = alg.search(multiple_search=True)
-            execution = datetime.now() - start_time
+            for algorithm, params in zip(algorithms, set_params):
 
-            info_dct['algorithm']     += [alg.name]
-            info_dct['reference_len'] += [len(reference)]
-            info_dct['candidate_len'] += [len(candidate)]
-            info_dct['preprocessing'] += [preprocess.total_seconds()]
-            info_dct['execution']     += [execution.total_seconds()]
-           # info_dct['observation']   += [observation]
-            info_dct['indexes']       += [str(indexes)]
+                start_time = datetime.now()
+                alg = algorithm(reference)
+                alg.set_candidate(candidate, **params)
+                preprocess = datetime.now() - start_time
+
+                start_time = datetime.now()
+                indexes    = alg.search(multiple_search=True)
+                execution  = datetime.now() - start_time
+
+                info_dct['algorithm']     += [alg.name]
+                info_dct['filename']      += [files_t[i]]
+                info_dct['reference_len'] += [len(reference)]
+                info_dct['candidate_len'] += [len(candidate)]
+                info_dct['preprocessing'] += [preprocess.total_seconds()]
+                info_dct['execution']     += [execution.total_seconds()]
+                info_dct['observation']   += [observation]
+                info_dct['indexes']       += [str(indexes)]
     return pd.DataFrame.from_dict(info_dct)
 
 
@@ -141,10 +145,10 @@ def get_plots(stat_df,
 
         p = plt.plot(ox, oy, '.-', label=alg)
         plt.fill_between(ox,
-                         oy - 3 * oy_std,
-                         oy + 3 * oy_std,
+                         oy - 2 * oy_std,
+                         oy + 2 * oy_std,
                          color=p[0].get_color(), alpha=0.3,
-                         label='Confidence interval of 95% ' + alg)
+                         label='Confidence interval of 68% ' + alg)
     plt.title(title)
     plt.xlabel('Reference string length')
     plt.ylabel('Time, seconds')
